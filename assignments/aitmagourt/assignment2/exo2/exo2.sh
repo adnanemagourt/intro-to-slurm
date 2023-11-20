b3ded99af396e88a4aaf8152ef312621d1adeece
aitmagourt adnane <adnane.aitmagourt@outlook.com>
2023-11-20 11:30:08 +0000

    exo2

--- assignments/aitmagourt/assignment2/exo2/exo2.sh
+++ assignments/aitmagourt/assignment2/exo2/exo2.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-#SBATCH --array=0-3%2
-
+#SBATCH --array=1-4
+#SBATCH --partition=special
 
 
 module purge
@@ -8,7 +8,12 @@ module load slurm/17.11.12
 module load OpenMPI/4.0.3-GCC-9.3.0
 module load Python/3.8.2-GCCcore-9.3.0
 
+echo "#!/bin/sh" >> exo2_$SLURM_ARRAY_TASK_ID.sh
+echo "#SBATCH --cores=$SLURM_ARRAY_TASK_ID" >> exo2_$SLURM_ARRAY_TASK_ID.sh
+echo "#SBATCH --output=exo2_$SLURM_ARRAY_TASK_ID\_out.png" >> exo2_$SLURM_ARRAY_TASK_ID.sh
+echo "#SBATCH --partition=special" >> exo2_$SLURM_ARRAY_TASK_ID.sh
+echo "TOTAL_POINTS=(0,1000,10000,100000,1000000)" >> exo2_$SLURM_ARRAY_TASK_ID.sh
+echo "python exo2.py \$TOTAL_POINTS[$SLURM_ARRAY_TASK_ID]" >> exo2_$SLURM_ARRAY_TASK_ID.sh
 
-total_points=(1000,10000,100000,1000000)
+sbatch exo2_$SLURM_ARRAY_TASK_ID.sh
 
-mpirun -n $SLURM_ARRAY_TASK_ID python exo2.py total_points[$SLURM_ARRAY_TASK_ID]
---
+++ assignments/aitmagourt/assignment2/exo2/exo2_4.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+#SBATCH --cores=4
+#SBATCH --output=exo2_4\_out.png
+#SBATCH --partition=special
+TOTAL_POINTS=(0,1000,10000,100000,1000000)
+python exo2.py $TOTAL_POINTS[4]
