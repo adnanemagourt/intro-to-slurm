a14711cdb12d1df0a9da417ad3385640522ce155
adnanemagourt <adnane.aitmagourt@outlook.com>
2023-11-27 12:42:11 +0100

    a

--- assignments/aitmagourt/assignment2/exo1/exo1.sh
+++ assignments/aitmagourt/assignment2/exo1/exo1.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-#SBATCH --array=1-128%1
+#SBATCH --array=1-128
 #SBATCH --output=exo1_out.txt
 
 
@@ -9,5 +9,5 @@ module load OpenMPI/4.0.3-GCC-9.3.0
 module load Python/3.8.2-GCCcore-9.3.0
 
 
-printf "#!/bin/sh \n#SBATCH --cores=%s \n#SBATCH --output=exo1_%s_out.txt \npython exo1.py" $SLURM_ARRAY_TASK_ID $SLURM_ARRAY_TASK_ID > exo1_$SLURM_ARRAY_TASK_ID.sh
+printf "#!/bin/sh \n#SBATCH --cores=%s \npython exo1.py >> exo1_out.txt" $SLURM_ARRAY_TASK_ID $SLURM_ARRAY_TASK_ID > exo1_$SLURM_ARRAY_TASK_ID.sh
 sbatch exo1_$SLURM_ARRAY_TASK_ID.sh
